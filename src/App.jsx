2f83ca50d5016d4209094b7cddaa8c57b237680c
Jhuan-Medeiros <jhuan.mcordeiro@gmail.com>
2024-03-18 16:57:11 -0300

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './App.css'
+
+
+const App = () => {
+  
+  const i
+  
+  return (
+    <div className='body'>
+    <div className='corpo'>
+      <div id='visor'></div>
+       <div className='linha1'>
+        <button className='botao' id='ac'> ac </button>
+        <button className='botao' id='1'> 1 </button>
+        <button className='botao' id='2'> 2 </button>
+        <button className='botao' id='3'> 3</button>
+      </div>
+      <div className='linha2'>
+        <button className='botao' id='4'> 4 </button>
+        <button className='botao' id='5'> 5</button>
+        <button className='botao' id='6'> 6 </button>
+        <button className='botao' id='7'> 7 </button>
+      </div>
+      <div className='linha3'>
+        <button className='botao' id='8'> 8 </button>
+        <button className='botao' id='9'> 9 </button>
+        <button className='botao' id='0'> 0 </button>
+        <button className='botao' id='+'> + </button>
+      </div>
+      <div className='linha4'>
+        <button className='botao' id='-'> - </button>
+        <button className='botao' id='*'> * </button>
+        <button className='botao' id='/'> / </button>
+        <button className='botao' id='='> = </button>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
